cfd2c9d13408da06df4f3718725203541d10b949
Jean Sérien <clavinova1@free.fr>
2024-09-01 14:14:45 +0200

    finalisation switch theme

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,3 +11,23 @@ if (flashbag) {
   });
 }
 
+//Application des themes (dark ...)
+const THEME_STORAGE_KEY = 'theme';
+const cachedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+
+const themePicker = document.querySelector('.theme-switcher');
+// if (!themePicker){ return false} ;
+
+const initialTheme = cachedTheme ?? 'auto';
+themePicker.querySelector('input[checked]').removeAttribute('checked');
+themePicker.querySelector(`input[value="${initialTheme}"]`).setAttribute('checked', '');
+
+themePicker.addEventListener('change', (e) => {
+  const theme = e.target.value;
+  if (theme === 'auto') {
+    localStorage.removeItem(THEME_STORAGE_KEY);
+  } else {
+    localStorage.setItem(THEME_STORAGE_KEY, theme);
+  }
+});
+
